c6a40d5144b0bde7f5d5b238f609003a0ae01497
patrickodey419 <patrickodey419@gmail.com>
2020-09-22 11:45:22 -0800

    chore: Made the app mobile responsive

--- app.js
+++ app.js
@@ -17,6 +17,23 @@
     let brazil = document.getElementById("brazil")
     let russia = document.getElementById("russia")
 
+    
+//usa cases
+        let usaTotalCases  = document.getElementById("usaTotalCases")
+        let usaActiveCases = document.getElementById("usaActiveCases")
+        let usaRecovered   = document.getElementById("usaRecovered")
+        let usaTotalDeaths = document.getElementById("usaTotalDeaths")
+
+//New confirmed
+        let usaNewConfirmed = document.getElementById("usaNewConfirmed")
+        let usaNewCases     = document.getElementById("usaNewCases")
+        let usaNewRecovered = document.getElementById("usaNewRecovered")
+        let usaNewDeaths    = document.getElementById("usaNewDeaths")        
+
+//Date
+        let date  = document.getElementById("date")
+        let date2 = document.getElementById("date2")     
+
 
 let url =  "https://api.covid19api.com/summary";
 let xhttp = new XMLHttpRequest();
@@ -25,28 +42,56 @@ xhttp.onreadystatechange = function(){
         let covidData = JSON.parse(this.responseText)
 
         //total cases data
-        totalCases.innerHTML     = covidData.Global.TotalConfirmed;
-        activeCases.innerHTML    = covidData.Global.TotalConfirmed - covidData.Global.TotalRecovered;
-        totalRecovered.innerHTML = covidData.Global.TotalRecovered;
-        totalDeaths.innerHTML    = covidData.Global.TotalDeaths;
+        totalCases.innerHTML     = Number(covidData.Global.TotalConfirmed).toLocaleString();
+        activeCases.innerHTML    = Number(covidData.Global.TotalConfirmed - covidData.Global.TotalRecovered).toLocaleString();
+        totalRecovered.innerHTML = Number(covidData.Global.TotalRecovered).toLocaleString();
+        totalDeaths.innerHTML    = Number(covidData.Global.TotalDeaths).toLocaleString();
 
         //new cases data
-        newConfirmed.innerHTML = `+${covidData.Global.NewConfirmed}`;
-        newCases.innerHTML     = `+${covidData.Global.NewConfirmed - covidData.Global.NewRecovered}`
-        newRecovered.innerHTML = `+${covidData.Global.NewRecovered}`;
-        newDeaths.innerHTML    = `+${covidData.Global.NewDeaths}`;
+        newConfirmed.innerHTML = `+${Number(covidData.Global.NewConfirmed).toLocaleString()}`;
+        newCases.innerHTML     = `+${Number(covidData.Global.NewConfirmed - covidData.Global.NewRecovered).toLocaleString()}`
+        newRecovered.innerHTML = `+${Number(covidData.Global.NewRecovered).toLocaleString()}`;
+        newDeaths.innerHTML    = `+${Number(covidData.Global.NewDeaths).toLocaleString()}`;
+
+        //top states
+        usa.innerHTML    = Number(covidData.Countries[179].TotalConfirmed).toLocaleString();
+        india.innerHTML  = Number(covidData.Countries[76].TotalConfirmed).toLocaleString();
+        brazil.innerHTML = Number(covidData.Countries[23].TotalConfirmed).toLocaleString();
+        russia.innerHTML = Number(covidData.Countries[139].TotalConfirmed).toLocaleString();
+
+        //usa cases
+        usaTotalCases.innerHTML  = Number(covidData.Countries[179].TotalConfirmed).toLocaleString();
+        usaActiveCases.innerHTML = Number(covidData.Countries[179].TotalConfirmed - covidData.Countries[179].TotalRecovered).toLocaleString();
+        usaRecovered.innerHTML   = Number(covidData.Countries[179].TotalRecovered).toLocaleString();
+        usaTotalDeaths.innerHTML = Number(covidData.Countries[179].TotalDeaths).toLocaleString();
+        usaNewCases.innerHTML    = Number(covidData.Countries[179].NewConfirmed).toLocaleString();
+        usaNewDeaths.innerHTML   = Number(covidData.Countries[179].NewDeaths).toLocaleString();
+
+        // date
+        let dateTrim = covidData.Date
+        dateTrim = dateTrim.split("T")[0]
+        date.innerHTML = `Updated: ${dateTrim}`;
+
+        // date2
+        let dateTrim2 = covidData.Date
+        dateTrim2 = dateTrim2.split("T")[0]
+        date2.innerHTML = `Updated: ${dateTrim2}`;
     }
 }
 
 xhttp.open("GET", url, true);
 xhttp.send();
 
+
+//second request
 let xhttp2 = new XMLHttpRequest()
 xhttp2.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-        let usaData = JSON.parse(this.responseText)
-        console.log(usaData) 
+        let data = JSON.parse(this.responseText)
+        console.log(data.Date)
+
+
     }
 }
-xhttp2.open("GET", "https://api.covid19api.com/countries/united-states", true)
+xhttp2.open("GET", "https://api.covid19api.com/summary", true)
 xhttp2.send()
